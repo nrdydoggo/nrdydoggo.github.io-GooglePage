995ce601f3d76163e8b7616c0d9523be28af8095
Jordan <84948603+StellaVulpis@users.noreply.github.com>
2022-11-22 18:11:41 +0900

    Add files via upload

---
+++ MusicPlayer/src/js/AudioPlayer.js
@@ -0,0 +1,66 @@
+// Range Slider
+// Range Slider
+
+let track_num = 0;
+let isPlaying = false;
+let updateTimer;
+let currentMusic = document.createElement('audio');
+let MusicList =
+[
+    {
+        name: "Die Anywhere Else",
+        artist: "Alec Holowka",
+        image: "https://github.com/StellaVulpis/Resources/raw/main/NITW-Cover.jpg",
+        path: "https://github.com/StellaVulpis/Resources/raw/main/NITW%20-%20Die%20Anywhere%20Else.mp3"
+    },
+    {
+        name: "Every Breath You Take",
+        artist: "The Police",
+        image: "https://github.com/StellaVulpis/Resources/raw/main/Every_Breath_You_Take-Cover.png",
+        path: "https://github.com/StellaVulpis/Resources/raw/main/Every%20Breath%20You%20Take.mp3"
+    },
+    {
+        name: "Everyone Is Horrid Except Me",
+        artist: "Quilloughby",
+        image: "https://github.com/StellaVulpis/Resources/raw/main/Everyone_Is_Horrid_Except_Me-Cover.jpg",
+        path: "https://github.com/StellaVulpis/Resources/raw/main/Everyone%20Is%20Horrid%20Except%20Me%20(And%20Possibly%20You).mp3"
+    },
+    {
+        name: "疾風伝",
+        artist: "Yasuharu Takanashi",
+        image: "https://github.com/StellaVulpis/Resources/raw/main/Shippuden-Cover.jpg",
+        path: "https://github.com/StellaVulpis/Resources/raw/main/Shippuden.mp3"
+    },
+    {
+        name: "DAGOTHWAVE",
+        artist: "young scrolls",
+        image: "https://github.com/StellaVulpis/Resources/raw/main/DAGOTHWAVE-Cover.png",
+        path: "https://github.com/StellaVulpis/Resources/raw/main/DAGOTHWAVE.mp3"
+    },
+];
+
+
+ToPrev()
+{
+
+}
+ToNext()
+{
+
+}
+PlayPause()
+{
+
+}
+SeekTo()
+{
+
+}
+SetVolume()
+{
+    
+}
+MuteVolume()
+{
+
+}
